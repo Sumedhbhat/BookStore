00d3a0459af2501f1966d30ff46d22b57db560ef
Sumedh Bhat <sumedhbhat01@gmail.com>
2022-05-14 11:58:53 +0530

    Environmental variable added

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -10,13 +10,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkOKoC9Jqk7pz0NJ6leidiVXdLRMxchiM",
-  authDomain: "bookstore-3ea4c.firebaseapp.com",
-  projectId: "bookstore-3ea4c",
-  storageBucket: "bookstore-3ea4c.appspot.com",
-  messagingSenderId: "515743768777",
-  appId: "1:515743768777:web:038a4d2a476a95c20fad18",
-  measurementId: "G-ZDSTW3F3RG",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
